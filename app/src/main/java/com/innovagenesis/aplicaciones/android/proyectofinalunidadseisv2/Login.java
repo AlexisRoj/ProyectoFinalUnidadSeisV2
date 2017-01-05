6fd0436997e2dad16388625dde1f36aab926a4e8
AlexisRoj <alexisroj2@gmail.com>
2017-01-05 16:44:08 -0600

    Cambios menores, parte uno inciada, creacion de pantalla login

--- app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadseisv2/Login.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadseisv2/Login.java
@@ -1,6 +1,7 @@
 package com.innovagenesis.aplicaciones.android.proyectofinalunidadseisv2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +12,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.innovagenesis.aplicaciones.android.proyectofinalunidadseisv2.Preference.PreferenceConstant;
+
 public class Login extends AppCompatActivity {
+
+
+    /** Administra las preferencias */
+    private SharedPreferences pref;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        pref = getSharedPreferences(PreferenceConstant.PREFERENCE_LOGIN, MODE_PRIVATE);
+        username = pref.getString(PreferenceConstant.PREF_KEY_USERNAME, null);
+
+
+        if (username != null){
+            cargarActivity();
+        }
 
 
         this.setTitle(R.string.title_Login);
@@ -30,27 +47,41 @@ public class Login extends AppCompatActivity {
             this.getWindow().setStatusBarColor(cambiarColor);
         }
 
-        final EditText userNameLogin = (EditText)findViewById(R.id.userNameLogin);
-        final EditText userNamePass = (EditText)findViewById(R.id.userNamePass);
+        final EditText userNameLogin = (EditText) findViewById(R.id.userNameLogin);
+        final EditText userNamePass = (EditText) findViewById(R.id.userNamePass);
 
-       Button button = (Button)findViewById(R.id.ingresarLogin);
+        Button button = (Button) findViewById(R.id.ingresarLogin);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /**
+                 *  variables para validar contenido del objeto, si algún elemento
+                 * se encuentra vacio, no permite la continuidad, ejecuta intent cuando
+                 * los dos elementos son falsos
+                 * */
                 String login = userNameLogin.getText().toString();
                 String pass = userNamePass.getText().toString();
 
-                if (login.isEmpty()||pass.isEmpty()){
+                if (login.isEmpty() || pass.isEmpty()) {
                     Toast.makeText(
-                            Login.this,"No pueden existir campos vacios",Toast.LENGTH_SHORT).show();
-                }else{
-                    Intent intent = new Intent(Login.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                            Login.this, "No pueden existir campos vacios", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    SharedPreferences.Editor edit = pref.edit();
+                    edit.putString(PreferenceConstant.PREF_KEY_USERNAME, login);
+                    edit.apply();
+
+                    cargarActivity();
                 }
 
             }
         });
 
-   }
+    }
+
+    private void cargarActivity() {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadseisv2/Preference/PreferenceConstant.java
@@ -0,0 +1,16 @@
+package com.innovagenesis.aplicaciones.android.proyectofinalunidadseisv2.Preference;
+
+/**
+ * Administra las preferences del login
+ * Created by alexi on 02/01/2017.
+ */
+
+public class PreferenceConstant {
+
+    public static final String PREFERENCE_LOGIN = "inicio_de_sesion";
+
+    public static final String PREF_KEY_USERNAME = "nombre_de_usuario";
+
+    public PreferenceConstant() {
+    }
+}
